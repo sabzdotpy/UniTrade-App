0a135f0286e60e1f10c42ec42fa5831d827670bb
sabzdotpy <incrediblesabari02@gmail.com>
2024-10-10 08:51:18 +0530

    add logout fn

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import './login_page.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  Future<void> _logout(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()), // Navigate to the login page after logout
+      );
+    } catch (e) {
+      print("Error during logout: $e");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Error logging out. Please try again."),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-          child: Text("Profile Page"),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _logout(context),
+          child: Text("Logout"),
         ),
+      ),
     );
   }
 }
